3366604d4aee915f9d742d613bc6893ad032c202
Roberto Ferreira <138168507+robertojff@users.noreply.github.com>
2025-02-24 14:54:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const menuItems = document.querySelectorAll('.menu-lista ul li');
 
-menuItems.forEach(item => {
-    item.addEventListener('click', () => {
-        menuItems.forEach(i => i.classList.remove('active')); // Remove a classe 'active' de todos os itens
-        item.classList.add('active'); // Adiciona a classe 'active' ao item clicado
-    });
-});
